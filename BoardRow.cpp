e30f1c70d431b2df145832f438e85c076d684d69
Kimberly <klindholm7@gmail.com>
2020-04-10 00:00:22 -0600

    updated boardRows moveObjects

--- BoardRow.cpp
+++ BoardRow.cpp
@@ -2,16 +2,27 @@
 
 #include "BoardRow.hpp"
 
-//notes for tmp
-//vectors
-//plants
-//zombies
-//bullets
-
 //moves stuff that needs to move
 //plants shoot instead of moving :)
 void BoardRow::moveObjects() {
+    for(int i=0;i<plants.size();++i) {
+	if(plants[i].readyToShoot()){
+	    PlantBullet tmp = plants[i].shoot();
+	    bullets.push_back(tmp);
+	}	
+    }
 
+    for(int i=0;i<bullets.size();++i) {
+	//if(bullets[i].readyToMove()) {
+	    //bullets[i].move();
+	//}
+    }
+
+    for(int i=0;i<zombies.size();++i) {
+	//if(zombies[i].readyToMove()){
+	//    zombies[i].move();
+	//}
+    }
 }
 
 //checks for collisions i.e. plants being eaten or zombies getting shot
