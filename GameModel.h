5d8412c18e74c441b99d4e22ae7ea5c2d307d916
gr <r.gavin.reid@aggiemail.usu.edu>
2020-04-10 20:27:04 -0600

    fixeroonies

--- GameModel.h
+++ GameModel.h
@@ -14,7 +14,7 @@ private:
     BoardRow myRow;	
 
 public:
-    GameModel(): zc(5,10) {fill();}
+    GameModel(): zc(1,10) {fill();}
     void fill(); //this is used to add a starting # of random plants until user input is a thing
     void Update();
     void Render();
